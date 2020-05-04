1f71a6993377f9916d5d8212f7a649e28c3a76a4
theSunAndSnow <55275586+theSunAndSnow@users.noreply.github.com>
2020-05-04 15:11:04 +0800

    为手机淘宝的二维码添加了点击关闭功能

---
+++ js/index.js
@@ -0,0 +1,9 @@
+var close_icon = document.querySelector
+    (".J_cup .J_cup-right .close_icon");
+
+var J_cup_right = document.querySelector
+(".J_cup  .J_cup-right");
+
+close_icon.onclick = function () {
+    J_cup_right.style.display = "none";
+}
